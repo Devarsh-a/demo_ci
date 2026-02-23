f0a8216cca02654b51252c8dbb06a9b25d9281fb
Devarsh-a <devarshnaik260@gmail.com>
2026-02-23 19:15:32 +0000

    first 1commit1

--- test_ui.py
+++ test_ui.py
@@ -4,23 +4,24 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import time
 
-options = Options()
-options.binary_location="/usr/bin/chromium"
-options.add_argument("--headless=new")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+def test_ui():
+    options = Options()
+    options.binary_location="/usr/bin/chromium"
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(service=Service("/usr/bin/chromedriver"), options=options)
+    driver = webdriver.Chrome(service=Service("/usr/bin/chromedriver"), options=options)
 
-driver.get("http://localhost:3000")
-time.sleep(1)
+    driver.get("http://localhost:3000")
+    time.sleep(1)
 
-driver.find_element(By.ID,"name").send_keys("Dev")
-driver.find_element(By.TAG_NAME,"button").click()
+    driver.find_element(By.ID,"name").send_keys("Dev")
+    driver.find_element(By.TAG_NAME,"button").click()
 
-time.sleep(1)
-text = driver.find_element(By.ID,"title").text
+    time.sleep(1)
+    text = driver.find_element(By.ID,"title").text
 
-assert "Dev" in text
+    assert "Dev" in text
 
-driver.quit()
+    driver.quit()
